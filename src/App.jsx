ebfcc51505eec7e6d1a2bc63c9e0f25665003e76
Dixon G <dxxn0101@gmail.com>
2023-03-30 10:34:24 -0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-center text-3xl">App</div>;
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,7 @@
+import BitconImg from "./assets/img/bitcon.png";
+import EthereumImg from "./assets/img/ethereum.png";
+import LitecoinImg from "./assets/img/litecoin.png";
+
+export const navData = [];
+
+export const currency = [];
