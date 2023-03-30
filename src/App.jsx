766ad9d3d3f8cd46606cd40f2a59f7bc402eacad
Dixon G <dxxn0101@gmail.com>
2023-03-30 14:30:28 -0600

    aos animation implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import { Hero } from "./components/Hero";
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    });
+  });
+
   return (
     <>
       <Hero />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,21 +8,33 @@ export const Hero = () => {
       <div className="container mx-auto">
         <div className="flex flex-col items-center lg:flex-row">
           <div className="flex-1">
-            <h1 className="font-bold leading-tight text-[32px] lg:text-[64px] mb-6">
+            <h1
+              className="font-bold leading-tight text-[32px] lg:text-[64px] mb-6"
+              data-aos="fade-down"
+              data-aos-delay="500"
+            >
               Fastest & secure platform to invest in crypto.
             </h1>
-            <p className="max-w-[440px] leading-relaxed mb-8">
+            <p
+              className="max-w-[440px] leading-relaxed mb-8"
+              data-aos="fade-down"
+              data-aos-delay="600"
+            >
               Buy and sell cryptocurrencies, trusted by 10M wallets with over
               $30 billion in transactions.
             </p>
-            <button className="btn gap-x-6 text-base lg:h-16">
+            <button
+              className="btn gap-x-6 text-base lg:h-16"
+              data-aos="fade-down"
+              data-aos-delay="700"
+            >
               Try for free
               <IoMdArrowDroprightCircle className="text-2xl lg:text-3xl" />
             </button>
           </div>
 
           <div className="flex-1">
-            <img src={HeroImg} alt="" data-aos="fade-up" />
+            <img src={HeroImg} alt="" data-aos="fade-up" data-aos-delay="600" />
           </div>
         </div>
       </div>
